7cd6b4bef6576388f34dc00be5da057a871bc719
Junaid <info@junaidkhan.dev>
2023-09-08 14:39:13 +0100

    svg

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import Button from "@/app/_components/Button/Button"
 
-
 import SecondaryBar from "./_components/SecondaryBar/SecondaryBar"
+import HomeCard from "./_components/ServiceCard/ServiceCard"
+import WorldCard from "./_components/WorldCard/WorldCard"
 
 import type { ThemeMode } from "@/types"
-import HomeCard from "./_components/HomeCard/Homecard"
-import WorldCard from "./_components/WorldCard/WorldCard"
 const HomePage = function () {
   const themeMode: ThemeMode = "neutral"
 
@@ -25,18 +24,35 @@ const HomePage = function () {
           <hr />
           <Button text="Button" themeMode={themeMode} />
           <section>
-        <div className="container px-6 m-auto">
-          <div className="grid grid-cols-4 gap-6 md:grid-cols-8 lg:grid-cols-12">
-            <div className="col-span-4 lg:col-span-6"> <HomeCard themeMode={themeMode}  /></div>
-            <div className="col-span-4 lg:col-span-6"> <HomeCard themeMode={themeMode}  /></div>
-            <div className="col-span-4 lg:col-span-6"> <HomeCard themeMode={themeMode}  /></div>
-            <div className="col-span-4 lg:col-span-6"> <HomeCard themeMode={themeMode}  /></div>
-            <div className="col-span-4 lg:col-span-6"> <WorldCard themeMode={themeMode}  /></div>
-            <div className="col-span-4 lg:col-span-6"> <HomeCard themeMode={themeMode}  /></div>
-          </div>
-        </div>
-      </section>
-
+            <div className="container px-6 m-auto">
+              <div className="grid grid-cols-4 gap-6 md:grid-cols-8 lg:grid-cols-12">
+                <div className="col-span-4 lg:col-span-6">
+                  {" "}
+                  <HomeCard themeMode={themeMode} />
+                </div>
+                <div className="col-span-4 lg:col-span-6">
+                  {" "}
+                  <HomeCard themeMode={themeMode} />
+                </div>
+                <div className="col-span-4 lg:col-span-6">
+                  {" "}
+                  <HomeCard themeMode={themeMode} />
+                </div>
+                <div className="col-span-4 lg:col-span-6">
+                  {" "}
+                  <HomeCard themeMode={themeMode} />
+                </div>
+                <div className="col-span-4 lg:col-span-6">
+                  {" "}
+                  <WorldCard themeMode={themeMode} />
+                </div>
+                <div className="col-span-4 lg:col-span-6">
+                  {" "}
+                  <HomeCard themeMode={themeMode} />
+                </div>
+              </div>
+            </div>
+          </section>
         </section>
       </main>
     </>
