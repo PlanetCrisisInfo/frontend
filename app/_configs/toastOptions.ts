2e33d0233f7f1ebbf2f81ab7858123a579b79417
junaid <junaid@aardy.com>
2023-12-12 15:10:42 +0000

    everything gone

--- app/_configs/toastOptions.ts
+++
@@ -1,9 +0,0 @@
-import { ToastOptions } from "react-toastify"
-
-export const toastSettings: ToastOptions = {
-  position: "top-right",
-  autoClose: 3000,
-  hideProgressBar: false,
-  closeOnClick: true,
-  draggable: true,
-}
