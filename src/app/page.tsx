f0cecf52fcece037a2a7c1d0909d83fa2e1df405
Junaid <info@junaidkhan.dev>
2023-12-13 12:23:42 +0000

    container init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,8 @@
-export default function Home() {
+export default function HomePage() {
   const theme = "sad"
   return (
-    <main>
-      <section className="sad bg">
-        <h1>Planet Crisis</h1>
-        <h2>Second Heading</h2>
-        <p>
-          Join Planet Crisis in our journey to save our beautiful Earth!
-          Discover easy tips, fun facts, and inspiring stories about fighting
-          climate change and protecting nature. Perfect for everyday heroes of
-          all ages who want to make a difference. Lets learn, share, and act
-          together for a happier, healthier planet!
-        </p>
-        <button className="sad link">Sad Link</button>
-      </section>
-      <section className="happy bg">
-        <h1>Planet Crisis</h1>
-        <p>
-          Join Planet Crisis in our journey to save our beautiful Earth!
-          Discover easy tips, fun facts, and inspiring stories about fighting
-          climate change and protecting nature. Perfect for everyday heroes of
-          all ages who want to make a difference. Lets learn, share, and act
-          together for a happier, healthier planet!
-        </p>
-        <button className="happy link">Sad Link</button>
-      </section>
-      <section className="neutral bg">
-        <h1>Planet Crisis</h1>
-        <p>
-          Join Planet Crisis in our journey to save our beautiful Earth!
-          Discover easy tips, fun facts, and inspiring stories about fighting
-          climate change and protecting nature. Perfect for everyday heroes of
-          all ages who want to make a difference. Lets learn, share, and act
-          together for a happier, healthier planet!
-        </p>
-        <button className="neutral link">Sad Link</button>
-      </section>
+    <main className="container border">
+      <h1>Hero Section</h1>
     </main>
   )
 }
