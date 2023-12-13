5ed4f55e01c200e7bc688295d9044680f80ba1b2
Junaid <info@junaidkhan.dev>
2023-12-13 17:01:37 +0000

    ellipses container

--- src/components/common/header/Hero/index.tsx
+++ src/components/common/header/Hero/index.tsx
@@ -7,23 +7,25 @@ interface IHeaderComponentProps {}
 
 const Hero = () => {
   return (
-    <header className="min-h-[90vh] gradient bg relative overflow-hidden">
-      <div className="w-[478px] absolute inset-0 z-0 object-contain -top-6 -left-40">
-        <Image
-          src={backEllipse}
-          alt="back ellipse illustration"
-          priority={true}
-          placeholder="empty"
-          className=""
-        />
-      </div>
-      <div className="w-[318px] absolute inset-0 z-0 object-contain -top-16 left-14 -rotate-12">
-        <Image
-          src={frontEllipse}
-          alt="front ellipse illustration"
-          priority={true}
-          placeholder="empty"
-        />
+    <header className="min-h-[90vh] gradient bg  overflow-hidden">
+      <div className="relative">
+        <div className="w-[478px] absolute inset-0 z-0 object-contain -top-[24px] -left-[160px]">
+          <Image
+            src={backEllipse}
+            alt="back ellipse illustration"
+            priority={true}
+            placeholder="empty"
+            className=""
+          />
+        </div>
+        <div className="w-[318px] absolute inset-0 z-0 object-contain -top-[60px] -left-[14px] -rotate-12">
+          <Image
+            src={frontEllipse}
+            alt="front ellipse illustration"
+            priority={true}
+            placeholder="empty"
+          />
+        </div>
       </div>
       <div className="container  border-4  relative z-20">
         <h1 className="">Hero Section</h1>
