9667f133561d24b579deee924e9d6071b0fd2ad2
Junaid <info@junaidkhan.dev>
2023-12-13 15:31:53 +0000

    circle position and index

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["images.unsplash.com", "assets.vercel.com"],
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "assets.vercel.com",
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
--- src/components/common/header/Hero/index.tsx
+++ src/components/common/header/Hero/index.tsx
@@ -7,12 +7,18 @@ interface IHeaderComponentProps {
 
 const Hero = ({ imageSrc, alt }: IHeaderComponentProps) => {
   return (
-    <header className="min-h-[80vh] gradient bg">
-      <div className="container  border-4 ">
-        <div className="max-w-[500px] circle-ils">
-          <Image src={imageSrc} alt={alt} priority={true} placeholder="empty" />
-        </div>
-        <h1>Hero Section</h1>
+    <header className="min-h-[80vh] gradient bg relative ">
+      <div className="max-w-[900px] circle-ils absolute inset-0 object-cover -left-52 -top-44 z-0">
+        <Image
+          src={imageSrc}
+          alt={alt}
+          priority={true}
+          placeholder="empty"
+          className="scale-x-[-1]"
+        />
+      </div>
+      <div className="container  border-4  relative z-10">
+        <h1 className="">Hero Section</h1>
       </div>
     </header>
   )
