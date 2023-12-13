2bf61561558c38c82f0000afe3b35c0241f77e16
Junaid <info@junaidkhan.dev>
2023-12-13 12:37:33 +0000

    test shift

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   const theme = "sad"
   return (
-    <main className="container border bg-blue-400 sm:bg-red-700 md:bg-orange-300 xl:bg-rose-400 3xl:bg-yellow-400">
+    <main>
       <h1>Hero Section</h1>
     </main>
   )
--- src/app/test.tsx
+++ src/app/test.tsx
@@ -36,6 +36,10 @@ export default function TestPage() {
         </p>
         <button className="neutral link">Sad Link</button>
       </section>
+
+      <section className="container border bg-blue-400 sm:bg-red-700 md:bg-orange-300 xl:bg-rose-400 3xl:bg-yellow-400">
+        <h1>Hero Section</h1>
+      </section>
     </main>
   )
 }
