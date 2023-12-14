7416ac80ae13bca10f2843f114b51c96d8c261f1
Martin Lowe <martinlowe673@gmail.com>
2023-12-14 08:17:41 +0000

    code ai a start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/common/header/Hero"
+import Services from "../components/common/Services"
 
 export default function HomePage() {
   const theme = "sad"
   return (
     <main>
       <Hero />
+      <Services  />
     </main>
   )
 }
