5934910a939f31954140d0dc547510be9ad85222
Junaid <info@junaidkhan.dev>
2023-12-14 10:19:25 +0000

    layout drama

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import Hero from "@/components/common/header/Hero"
+import Ellipses from "@/components/common/header/home/Ellipses"
+import Hero from "@/components/common/header/home/Hero"
 
 export default function HomePage() {
   const theme = "sad"
   return (
-    <main>
+    <>
       <Hero />
-    </main>
+      <Ellipses />
+      <section>
+        <div className="container">
+          <h1 className="">Home Page</h1>
+        </div>
+      </section>
+    </>
   )
 }
---
+++ src/components/common/header/home/Hero/index.tsx
@@ -0,0 +1,13 @@
+interface IHeaderComponentProps {}
+
+const Hero = () => {
+  return (
+    <div className="min-h-[90vh] gradient bg  overflow-hidden">
+      <div className="container  border-4  relative z-20">
+        <h1 className="">Hero Section</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
