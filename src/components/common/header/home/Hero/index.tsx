5852b74f3d47f4ecf550b68a544a5133f1a70e1c
Junaid <info@junaidkhan.dev>
2023-12-22 15:57:39 +0000

    fix the hero planet svg

--- src/components/common/header/home/Hero/index.tsx
+++ src/components/common/header/home/Hero/index.tsx
@@ -1,15 +1,21 @@
+import Image from "next/image"
+
+import planetSvg from "@/../public/images/planet.svg"
+
 import styles from "./Hero.module.scss"
 
 interface IHeaderComponentProps {}
 
 const Hero = () => {
   return (
-    <div className="relative -mt-1">
-      <div className="min-h-[80vh] bg gradient border border-yellow-400"></div>
-      <div className={`${styles.planetSvg}`}></div>
-      {/* <div className="container  border-4 absolute inset-0 z-20">
-        <h1>Hero Section</h1>
-      </div> */}
+    <div className="">
+      <Image src={planetSvg} alt="planet" className={styles.planetSvg} />
+      {/* <p className="flex justify-end items-center text-3xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+        accusamus perspiciatis ratione dolore quidem quibusdam ducimus quos,
+        repellendus debitis, veniam consequatur excepturi est aut nisi.
+        Accusamus explicabo similique repellendus laudantium?
+      </p> */}
     </div>
   )
 }
