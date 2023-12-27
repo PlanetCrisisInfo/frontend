aea5394283cdaf1df44aef3abeed3bcd152f46ce
Junaid <info@junaidkhan.dev>
2023-12-27 18:25:12 +0000

    will be back

--- src/components/common/header/home/Hero/index.tsx
+++ src/components/common/header/home/Hero/index.tsx
@@ -7,7 +7,7 @@ import styles from "./Hero.module.scss"
 interface IHeaderComponentProps {}
 const Hero = () => {
   return (
-    <div className="w-[100vw] max-w-[2160px] mx-auto heroHome bg">
+    <div className="w-[100vw] max-w-[2160px] mx-auto heroHome bg ">
       <Image
         src={planetSvg}
         alt="planet"
@@ -15,12 +15,14 @@ const Hero = () => {
         priority={true}
         quality={75}
       />
-      {/* <p className="flex justify-end items-center text-3xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
-        accusamus perspiciatis ratione dolore quidem quibusdam ducimus quos,
-        repellendus debitis, veniam consequatur excepturi est aut nisi.
-        Accusamus explicabo similique repellendus laudantium?
-      </p> */}
+      <div className="">
+        <p className="text-3xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          accusamus perspiciatis ratione dolore quidem quibusdam ducimus quos,
+          repellendus debitis, veniam consequatur excepturi est aut nisi.
+          Accusamus explicabo similique repellendus laudantium?
+        </p>
+      </div>
     </div>
   )
 }
