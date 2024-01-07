1d57cf9e048b7398d37d60f5a9e729be8c958b7e
Martin Lowe <martinlowe673@gmail.com>
2024-01-07 18:12:39 +0000

    amended heights on font awesome and icons

--- src/components/common/header/home/Services/index.tsx
+++ src/components/common/header/home/Services/index.tsx
@@ -1,3 +1,5 @@
+import { faBiohazard, faHouseFloodWater, faSunPlantWilt, faTornado, faVolcano, faWheatAwnCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image"
 import { FaLongArrowAltRight } from "react-icons/fa";
 
@@ -16,19 +18,12 @@ const Services = ()  =>{
         <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
           <div className="flex flex-col items-stretch w-[36%] max-md:w-full max-md:ml-0">
             <div className="flex grow flex-col max-md:mt-10">
-              <Image
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/7063c0a2f9ef6f15bcc75c90d7f1b5732ccd29892f725fbe64d3dc3969d04b57?"
-              className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full mt-12 max-md:mt-10"
-                width={300}
-                height={300}
-                alt="image"
-              />
+            <FontAwesomeIcon icon={faSunPlantWilt}  height={200} className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full"/>
               <div className="text-zinc-600 text-center text-base leading-7 self-center whitespace-nowrap mt-7">
                 Earth Service 1
               </div>
               <div className="text-zinc-900 text-center text-2xl font-medium leading-9 capitalize self-stretch mt-4">
-                For marketing professionals research online.
+               Find out more here,
               </div>
               <div className="self-center flex items-center gap-2 mt-8">
               <FaLongArrowAltRight />
@@ -38,19 +33,12 @@ const Services = ()  =>{
                 </div>
               </div>
 
-              <Image
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/23965a51e8e4becd18cfbb1e2f46669b655402a52459feaf921fd4b26eabeb8b?"
-                className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full mt-12 max-md:mt-10"
-                width={300}
-                height={300}
-                alt="image"
-              />
+              <FontAwesomeIcon icon={faVolcano}  width={300} height={200} className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full"/>
               <div className="text-zinc-600 text-center text-base leading-7 self-center whitespace-nowrap mt-7">
               Earth Service 2
               </div>
               <div className="text-zinc-900 text-center text-2xl font-medium leading-9 capitalize self-stretch mt-4">
-                For marketing professionals research online.
+              Find out more here,
               </div>
               <div className="self-center flex items-center gap-2 mt-8">
               <FaLongArrowAltRight />
@@ -63,20 +51,12 @@ const Services = ()  =>{
           </div>
           <div className="flex flex-col items-stretch w-[34%] ml-5 max-md:w-full max-md:ml-0">
             <div className="flex grow flex-col max-md:mt-10">
-              <Image
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/492bb7b6d70b1f2c3c9327c7d5aeba8a399e47b00d68182c8c29ec3382c01e33?"
-
-                className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full mt-12 max-md:mt-10"
-                width={300}
-                height={300}
-                alt="image"
-              />
+            <FontAwesomeIcon icon={faTornado}  width={300} height={200} className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full"/> 
               <div className="text-zinc-600 text-center text-base leading-7 self-center whitespace-nowrap mt-7">
               Earth Service 3
               </div>
               <div className="text-zinc-900 text-center text-2xl font-medium leading-9 capitalize self-stretch mt-4">
-                for marketing provided do email and phone sMS
+              Find out more here,
               </div>
               <div className="self-center flex items-center gap-2 mt-8">
               <FaLongArrowAltRight />
@@ -85,19 +65,12 @@ const Services = ()  =>{
                   Read More
                 </div>
               </div>
-              <Image
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/d8282ff7fd36089d719cdc8127f671d783d6550341f161392bd38469f9f50efd?"
-                className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full mt-12 max-md:mt-10"
-                width={300}
-                height={300}
-                alt="image"
-              />
+              <FontAwesomeIcon icon={faWheatAwnCircleExclamation}  width={300} height={200} className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full"/>
               <div className="text-zinc-600 text-center text-base leading-7 self-center whitespace-nowrap mt-7">
               Earth Service 4
               </div>
               <div className="text-zinc-900 text-center text-2xl font-medium leading-9 capitalize self-stretch mt-4">
-                for marketing provided do email and phone sMS
+              Find out more here,
               </div>
               <div className="self-center flex items-center gap-2 mt-8">
               <FaLongArrowAltRight />
@@ -110,19 +83,12 @@ const Services = ()  =>{
           </div>
           <div className="flex flex-col items-stretch w-[31%] ml-5 max-md:w-full max-md:ml-0">
             <div className="flex grow flex-col max-md:mt-10">
-              <Image
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/7f980187c2a8398df31b31f534fe425cd11b9cd64015b988b87672ed09e0eba7?"
-                className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full mt-12 max-md:mt-10"
-                width={300}
-                height={300}
-                alt="image"
-              />
+            <FontAwesomeIcon icon={faHouseFloodWater}  width={300} height={200} className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full"/>
               <div className="text-zinc-600 text-center text-base leading-7 self-center whitespace-nowrap mt-7">
               Earth Service 5
               </div>
               <div className="text-zinc-900 text-center text-2xl font-medium leading-9 capitalize self-stretch mt-4">
-                For marketing professionals research online.
+              Find out more here,
               </div>
               <div className="self-center flex items-center gap-2 mt-8">
               <FaLongArrowAltRight />
@@ -130,19 +96,12 @@ const Services = ()  =>{
                   Read More
                 </div>
               </div>
-              <Image
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/6155ce25a052f68cd40e70652714a22ac3ef28f9c9b0b39bad0b7a594d75d160?"
-                className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full mt-12 max-md:mt-10"
-                width={300}
-                height={300}
-                alt="image"
-              />
+              <FontAwesomeIcon icon={faBiohazard}  width={300} height={200} className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full"/>
               <div className="text-zinc-600 text-center text-base leading-7 self-center whitespace-nowrap mt-7">
               Earth Service 6
               </div>
               <div className="text-zinc-900 text-center text-2xl font-medium leading-9 capitalize self-stretch mt-4">
-                For marketing professionals research online.
+              Find out more here,
               </div>
               <div className="self-center flex items-center gap-2 mt-8">
               <FaLongArrowAltRight />
