1523963ff87ca16ee74afd744e0179c5de9246b2
Martin Lowe <martinlowe673@gmail.com>
2024-01-07 17:02:25 +0000

    added arrow on

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com", "assets.vercel.com", "daisyui.com"],
     formats: ["image/avif", "image/webp"],
+    dangerouslyAllowSVG:true,
     remotePatterns: [
       {
         protocol: "https",
@@ -16,6 +17,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "daisyui.com",
       },
+      {
+        protocol:"https",
+        hostname:"cdn.builder.io"
+      }
     ],
   },
 }
---
+++ src/components/common/header/home/Services/index.tsx
@@ -0,0 +1,161 @@
+import Image from "next/image"
+import { FaLongArrowAltRight } from "react-icons/fa";
+
+const Services = ()  =>{
+  return(
+    <div className="flex flex-col items-stretch px-5">
+      <div className="text-zinc-900 text-center text-5xl font-bold leading-[61.88px] capitalize self-center max-md:max-w-full max-md:text-4xl">
+        Best of our service
+      </div>
+      <div className="text-zinc-600 text-center text-base leading-7 self-center max-w-[594px] mt-8 max-md:max-w-full">
+        Integer vehicula diam et sollicitudin scelerisque. Vestibulum
+        pellentesque bibendum magna, et bibendum turpis lacinia id. Praesent
+        egestas tincidunt risus.
+      </div>
+      <div className="w-full mt-14 max-md:max-w-full max-md:mt-10">
+        <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+          <div className="flex flex-col items-stretch w-[36%] max-md:w-full max-md:ml-0">
+            <div className="flex grow flex-col max-md:mt-10">
+              <Image
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/7063c0a2f9ef6f15bcc75c90d7f1b5732ccd29892f725fbe64d3dc3969d04b57?"
+              className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full mt-12 max-md:mt-10"
+                width={300}
+                height={300}
+                alt="image"
+              />
+              <div className="text-zinc-600 text-center text-base leading-7 self-center whitespace-nowrap mt-7">
+                Earth Service 1
+              </div>
+              <div className="text-zinc-900 text-center text-2xl font-medium leading-9 capitalize self-stretch mt-4">
+                For marketing professionals research online.
+              </div>
+              <div className="self-center flex items-center gap-2 mt-8">
+              <FaLongArrowAltRight />
+
+                <div className="text-zinc-600 text-base leading-7 self-stretch">
+                  Read More
+                </div>
+              </div>
+
+              <Image
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/23965a51e8e4becd18cfbb1e2f46669b655402a52459feaf921fd4b26eabeb8b?"
+                className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full mt-12 max-md:mt-10"
+                width={300}
+                height={300}
+                alt="image"
+              />
+              <div className="text-zinc-600 text-center text-base leading-7 self-center whitespace-nowrap mt-7">
+              Earth Service 2
+              </div>
+              <div className="text-zinc-900 text-center text-2xl font-medium leading-9 capitalize self-stretch mt-4">
+                For marketing professionals research online.
+              </div>
+              <div className="self-center flex items-center gap-2 mt-8">
+              <FaLongArrowAltRight />
+
+                <div className="text-zinc-600 text-base leading-7 self-stretch">
+                  Read More
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-stretch w-[34%] ml-5 max-md:w-full max-md:ml-0">
+            <div className="flex grow flex-col max-md:mt-10">
+              <Image
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/492bb7b6d70b1f2c3c9327c7d5aeba8a399e47b00d68182c8c29ec3382c01e33?"
+
+                className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full mt-12 max-md:mt-10"
+                width={300}
+                height={300}
+                alt="image"
+              />
+              <div className="text-zinc-600 text-center text-base leading-7 self-center whitespace-nowrap mt-7">
+              Earth Service 3
+              </div>
+              <div className="text-zinc-900 text-center text-2xl font-medium leading-9 capitalize self-stretch mt-4">
+                for marketing provided do email and phone sMS
+              </div>
+              <div className="self-center flex items-center gap-2 mt-8">
+              <FaLongArrowAltRight />
+
+                <div className="text-zinc-600 text-base leading-7 self-stretch">
+                  Read More
+                </div>
+              </div>
+              <Image
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/d8282ff7fd36089d719cdc8127f671d783d6550341f161392bd38469f9f50efd?"
+                className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full mt-12 max-md:mt-10"
+                width={300}
+                height={300}
+                alt="image"
+              />
+              <div className="text-zinc-600 text-center text-base leading-7 self-center whitespace-nowrap mt-7">
+              Earth Service 4
+              </div>
+              <div className="text-zinc-900 text-center text-2xl font-medium leading-9 capitalize self-stretch mt-4">
+                for marketing provided do email and phone sMS
+              </div>
+              <div className="self-center flex items-center gap-2 mt-8">
+              <FaLongArrowAltRight />
+
+                <div className="text-zinc-600 text-base leading-7 self-stretch">
+                  Read More
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-stretch w-[31%] ml-5 max-md:w-full max-md:ml-0">
+            <div className="flex grow flex-col max-md:mt-10">
+              <Image
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/7f980187c2a8398df31b31f534fe425cd11b9cd64015b988b87672ed09e0eba7?"
+                className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full mt-12 max-md:mt-10"
+                width={300}
+                height={300}
+                alt="image"
+              />
+              <div className="text-zinc-600 text-center text-base leading-7 self-center whitespace-nowrap mt-7">
+              Earth Service 5
+              </div>
+              <div className="text-zinc-900 text-center text-2xl font-medium leading-9 capitalize self-stretch mt-4">
+                For marketing professionals research online.
+              </div>
+              <div className="self-center flex items-center gap-2 mt-8">
+              <FaLongArrowAltRight />
+                <div className="text-zinc-600 text-base leading-7 self-stretch">
+                  Read More
+                </div>
+              </div>
+              <Image
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/6155ce25a052f68cd40e70652714a22ac3ef28f9c9b0b39bad0b7a594d75d160?"
+                className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full mt-12 max-md:mt-10"
+                width={300}
+                height={300}
+                alt="image"
+              />
+              <div className="text-zinc-600 text-center text-base leading-7 self-center whitespace-nowrap mt-7">
+              Earth Service 6
+              </div>
+              <div className="text-zinc-900 text-center text-2xl font-medium leading-9 capitalize self-stretch mt-4">
+                For marketing professionals research online.
+              </div>
+              <div className="self-center flex items-center gap-2 mt-8">
+              <FaLongArrowAltRight />
+                <div className="text-zinc-600 text-base leading-7 self-stretch">
+                  Read More
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Services
+
