a4395ff7f6ec710145f85314a3b9b7114adc425d
Martin Lowe <martinlowe673@gmail.com>
2024-01-08 07:37:13 +0000

    icons now have relevance to names

--- src/components/common/header/home/Services/index.tsx
+++ src/components/common/header/home/Services/index.tsx
@@ -19,10 +19,10 @@ const Services = ()  =>{
           <div className="flex flex-col items-stretch w-[36%] max-md:w-full max-md:ml-0">
             <div className="flex grow flex-col max-md:mt-10">
             <FontAwesomeIcon icon={faSunPlantWilt}  height={200} className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full"/>
-              <div className="text-zinc-600 text-center text-base leading-7 self-center whitespace-nowrap mt-7">
-                Earth Service 1
+              <div className="text-zinc-600  text-2xl text-center  leading-7 self-center whitespace-nowrap mt-7">
+                High Temperatures
               </div>
-              <div className=" text-center text-2xl font-medium leading-9 capitalize self-stretch mt-4">
+              <div className=" text-center font-medium leading-9 capitalize self-stretch mt-4">
                Find out more here,
               </div>
               <div className="self-center flex items-center gap-2 mt-8">
@@ -34,10 +34,10 @@ const Services = ()  =>{
               </div>
 
               <FontAwesomeIcon icon={faVolcano}  width={300} height={200} className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full"/>
-              <div className="text-zinc-600 text-center text-base leading-7 self-center whitespace-nowrap mt-7">
-              Earth Service 2
+              <div className="text-zinc-600  text-2xl text-center  leading-7 self-center whitespace-nowrap mt-7">
+              Volcanoes
               </div>
-              <div className="text-zinc-900 text-center text-2xl font-medium leading-9 capitalize self-stretch mt-4">
+              <div className="text-zinc-900 text-center  font-medium leading-9 capitalize self-stretch mt-4">
               Find out more here,
               </div>
               <div className="self-center flex items-center gap-2 mt-8">
@@ -52,10 +52,10 @@ const Services = ()  =>{
           <div className="flex flex-col items-stretch w-[34%] ml-5 max-md:w-full max-md:ml-0">
             <div className="flex grow flex-col max-md:mt-10">
             <FontAwesomeIcon icon={faTornado}  width={300} height={200} className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full"/> 
-              <div className="text-zinc-600 text-center text-base leading-7 self-center whitespace-nowrap mt-7">
-              Earth Service 3
+              <div className="text-zinc-600 text-2xl text-center leading-7 self-center whitespace-nowrap mt-7">
+              Tornados
               </div>
-              <div className="text-zinc-900 text-center text-2xl font-medium leading-9 capitalize self-stretch mt-4">
+              <div className="text-zinc-900 text-center  font-medium leading-9 capitalize self-stretch mt-4">
               Find out more here,
               </div>
               <div className="self-center flex items-center gap-2 mt-8">
@@ -66,10 +66,10 @@ const Services = ()  =>{
                 </div>
               </div>
               <FontAwesomeIcon icon={faWheatAwnCircleExclamation}  width={300} height={200} className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full"/>
-              <div className="text-zinc-600 text-center text-base leading-7 self-center whitespace-nowrap mt-7">
-              Earth Service 4
+              <div className="text-zinc-600 text-center text-2xl leading-7 self-center whitespace-nowrap mt-7">
+              Famine
               </div>
-              <div className="text-zinc-900 text-center text-2xl font-medium leading-9 capitalize self-stretch mt-4">
+              <div className="text-zinc-900 text-center  font-medium leading-9 capitalize self-stretch mt-4">
               Find out more here,
               </div>
               <div className="self-center flex items-center gap-2 mt-8">
@@ -84,10 +84,10 @@ const Services = ()  =>{
           <div className="flex flex-col items-stretch w-[31%] ml-5 max-md:w-full max-md:ml-0">
             <div className="flex grow flex-col max-md:mt-10">
             <FontAwesomeIcon icon={faHouseFloodWater}  width={300} height={200} className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full"/>
-              <div className="text-zinc-600 text-center text-base leading-7 self-center whitespace-nowrap mt-7">
-              Earth Service 5
+              <div className="text-zinc-600 text-center text-2xl leading-7 self-center whitespace-nowrap mt-7">
+              Floods
               </div>
-              <div className="text-zinc-900 text-center text-2xl font-medium leading-9 capitalize self-stretch mt-4">
+              <div className="text-zinc-900 text-center  font-medium leading-9 capitalize self-stretch mt-4">
               Find out more here,
               </div>
               <div className="self-center flex items-center gap-2 mt-8">
@@ -97,10 +97,10 @@ const Services = ()  =>{
                 </div>
               </div>
               <FontAwesomeIcon icon={faBiohazard}  width={300} height={200} className="aspect-[1.11] object-contain object-center w-[88px] overflow-hidden self-center max-w-full"/>
-              <div className="text-zinc-600 text-center text-base leading-7 self-center whitespace-nowrap mt-7">
-              Earth Service 6
+              <div className="text-zinc-600 text-2xl text-center  leading-7 self-center whitespace-nowrap mt-7">
+              Nuclear War
               </div>
-              <div className="text-zinc-900 text-center text-2xl font-medium leading-9 capitalize self-stretch mt-4">
+              <div className="text-zinc-900 text-center font-medium leading-9 capitalize self-stretch mt-4">
               Find out more here,
               </div>
               <div className="self-center flex items-center gap-2 mt-8">
